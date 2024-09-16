acc2d8a2a1ba8d6fd6a04ee608bb47b1c04512c1
Sanjana-17-p <sanjanahlg@gmail.com>
2024-09-16 17:37:17 +0530

    Add files via upload

---
+++ 01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 213525
+let accountEmail = "sanjana@gmail.com"
+var accountPassword = "11223"
+accountcity = "uttarpardesh"
+let accountState;
+//accountId = 46364 //not allowed
+accountEmail = "kiri@gmail.com"
+accountPassword = "35368"
+accountcity = "delhi"
+
+//console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState]);
+/* prefer not use var because of issue of block scope and functional scope*/
