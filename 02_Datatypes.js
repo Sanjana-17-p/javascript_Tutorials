dea1bce175e58d620d4ece1977dff7386b104cd9
Sanjana-17-p <sanjanahlg@gmail.com>
2024-09-17 16:56:28 +0530

    Add files via upload

---
+++ 02_Datatypes.js
@@ -0,0 +1,24 @@
+//data types => Primitive Datatypes
+//          => Non Primitive Datatypes
+/*  1) Primitive DataTypes
+    Number=>
+    String => ""
+    Symbol => unique
+    Boolean=> True/False
+    BigInt
+    Undefined
+    Null
+
+    2) Non Primitive Datatypes
+     Object
+
+*/
+ let name = "Tom"
+ let age = 18
+ let isLoggedIn = false
+
+ console.log(typeof "tom");
+ console.log(typeof age);
+ console.log(typeof null);   // object
+ console.log(typeof undefined); // undefined
+ stringIndexof(Tom,o,1);
