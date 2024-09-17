68e8e7eafe72aa03352f23d984b808dd86598cc9
Sanjana-17-p <sanjanahlg@gmail.com>
2024-09-17 16:56:49 +0530

    Add files via upload

---
+++ 03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "11";
+let age = "33abd";
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(age)
+//console.log(typeof valueInNumber); //number
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber) //NaN
+
+//let va1  = null; //  0
+ let va1  = undefined;//  NaN
+
+
+// let val_In_Number = Number(va1)
+ //console.log(typeof val_In_Number)
+ //console.log(val_In_Number) 
+
+
+ /* Summary
+  "33" => 33
+  "33abd" => NaN
+  true = 1;
+  false = 0;
+  null = 0
+  undefined =  NaN
+*/  
+let isloggedIn = 1;
+let booleanIsloggedIn =Boolean(isloggedIn)
+console.log(booleanIsloggedIn);
