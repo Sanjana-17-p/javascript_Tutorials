6ea6cc6a4bae686a65aa22abab19b9b2d56af6f6
Sanjana <sanjanahlg@gmail.com>
2024-09-19 17:11:14 +0530

    Add files via upload

---
+++ 07_number&math.js
@@ -0,0 +1,43 @@
+const score = 400
+//console.log(score)
+
+const balance = new Number(2999)
+//console.log(balance)  // output== [Number: 2999] 
+
+//console.log(balance.toString().length) 
+//console.log(balance.toFixed(2)) //use for e-commerce application.
+  
+const otherNumber= 23.7989
+//console.log(otherNumber.toPrecision(3));
+//console.log(otherNumber.toPrecision(2));
+
+const num1 = 123.79757
+//console.log(num1.toPrecision(3));
+//console.log(num1.toPrecision(1));
+
+
+const num2  = 1000000
+//console.log(num2.toLocaleString('en-IN'));
+
+
+//*************** MATHS ************//
+
+console.log(Math);
+//console.log(Math.abs(-8));  // 8
+//console.log(Math.round(5.6)); //6
+// console.log(Math.ceil(4.2));  //5
+ console.log(Math.floor(4.2));
+// console.log(Math.min(4,2,6,1,0,9));
+// console.log(Math.max(4,2,6,1,0,9));
+
+console.log((Math.random()*10) + 1);  // random gives the value between 0 to 1.
+   
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min + 1))+min);
+
+
+
+
+
