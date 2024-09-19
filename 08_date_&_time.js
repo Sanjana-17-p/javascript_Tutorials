0220d54dca62e66b928037b8a19f38fe0896c120
Sanjana <sanjanahlg@gmail.com>
2024-09-19 17:12:02 +0530

    Add files via upload

---
+++ 08_date_&_time.js
@@ -0,0 +1,34 @@
+// Dates
+let my_Date = new Date()
+console.log(my_Date.toString());
+
+console.log(typeof my_Date); // object
+ 
+
+// console.log(my_Date.toLocaleDateString());   //19/9/2024
+// console.log(my_Date.toDateString()); //Thu Sep 19 2024
+// console.log(my_Date.toISOString()); // 2024-09-19T11:19:19.184Z
+// console.log(my_Date.toLocaleString()); //19/9/2024, 4:50:31 pm
+
+
+ let myCreatedDate = new Date(2024, 0, 12)
+// //console.log(myCreatedDate.toDateString());  //Fri Jan 12 2024
+
+
+// let myCreatedDate1 = new Date(2024, 0, 12,5,6,4)
+// console.log(myCreatedDate1.toLocaleString()); //12/1/2024, 5:06:04 am
+//
+
+// timestamp
+  let myTimeStamp = Date.now()
+//   console.log(myTimeStamp);
+//   console.log(myCreatedDate.getTime());
+  
+  let newDate = new Date()
+  console.log(newDate);
+  //console.log(newDate.getMonth());
+  
+  
+  
+
+  
