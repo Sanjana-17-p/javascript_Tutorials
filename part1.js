271c16254e259fac64a25df486241965d0510bf7
Sanjana <sanjanahlg@gmail.com>
2024-09-25 08:16:23 +0530

    Add files via upload

---
+++ part1.js
@@ -0,0 +1,28 @@
+// // if and else
+// // conditional operator-  <,>,<=,>=,==,===,!=,!==
+   
+
+// // const  temperature = 45
+
+// // if(temperature>50){
+// //     console.log("temperature is high");  
+// // }
+// // else{
+// //     console.log("temperature not much high");
+// // }
+//  const score= 200
+//  if(score>100){
+//     const power ="fly"
+//     // var power= "fly"  // var has global scope
+//     console.log(`user power:${power}`);
+    
+//  }
+// //  console.log(`user power:${power}`);// ReferenceError: power is not defined console.log(`user power:${power}`);
+// //  console.log(`user power:${power}`);  
+
+// const balance = 1000
+// // if(balance>500) console.log("test");// valid
+// // if(balance>500) console.log("test"),console.log("test2");-----> not good practice
+
+
+
