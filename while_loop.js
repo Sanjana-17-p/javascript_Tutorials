e984142c6b8017d2b1df0ca6b96db7de9131f05e
Sanjana <sanjanahlg@gmail.com>
2024-09-26 19:54:38 +0530

    Add files via upload

---
+++ while_loop.js
@@ -0,0 +1,22 @@
+// while
+let i=1
+while (i<10) {
+    console.log(i);
+    i++
+    
+}
+
+let myArr = ["Harry","Ron","hermoine"]
+let arr= 0
+while (arr<myArr.length) {
+    console.log(`value is ${arr} = ${myArr[arr]}`);
+    arr = arr+1
+    
+}
+// do while
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score<5);
