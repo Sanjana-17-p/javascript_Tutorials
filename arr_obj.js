bc52f058e29bde561576f64458db5e7f56298f41
Sanjana <sanjanahlg@gmail.com>
2024-09-26 19:53:38 +0530

    Add files via upload

---
+++ arr_obj.js
@@ -0,0 +1,24 @@
+// [{},{},{}]
+
+
+const myCoding=[{
+    languageName:"javascript",
+    filename:"js"
+},
+{
+    lang :"cpp",
+    filename:"c++"
+
+
+},
+{
+
+}
+]
+myCoding.forEach((item)=>{
+    console.log(item.filename);
+    console.log(item.lang);
+    
+
+}
+)
